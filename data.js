70ca804c0b1b34e6ca056b087518ef31079ca5b6
ENNIK11 <benezul7@gmail.com>
2025-05-13 11:33:40 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,641 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-v0230027",
+      "name": "V0230027",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": -1.3769487165547787,
+        "pitch": 0,
+        "fov": 1.2311558017980793
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07196266596288226,
+          "pitch": 0.020213084713455842,
+          "rotation": 6.283185307179586,
+          "target": "1-v0240028"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2129865813464598,
+          "pitch": -0.4721314950245272,
+          "title": "Benvinguts a l’Arxiu Històric de Sabadell AHS",
+          "text": "<p data-start=\"90\" data-end=\"232\" class=\"\">L’Arxiu Històric de Sabadell (AHS) és un servei de l’Ajuntament dedicat a preservar, gestionar i difondre el patrimoni documental de Sabadell.</p>\n<p data-start=\"234\" data-end=\"429\" class=\"\">L’AHS disposa de diversos dipòsits, amb una capacitat aproximada de 5.000 metres lineals de documents de tot tipus de format i suport: pergamins, cartells, plànols, fotografies, pel·lícules, etc.</p>\n<p data-start=\"431\" data-end=\"868\" class=\"\">L’arxiu custodia documents que abasten des del segle XII fins avui, amb especial rellevància per als fons dels segles XVI a XX, èpoques en què es desenvolupen l’àmbit polític, social, industrial i urbà de la ciutat. Entre els seus fons destaca l’arxiu municipal, amb documentació des del segle XIV. La documentació municipal arriba pràcticament fins als nostres dies, de manera que té una doble funcionalitat, històrica i administrativa.</p>\n<p data-start=\"870\" data-end=\"1250\" class=\"\">Així mateix, l’AHS és dipositari d’altres fons que no són més enllà de l’àmbit exclusivament sabadellenc. Aquest és el cas del fons del Districte Notarial de Sabadell —integrat per volums des de l’any 1401— o bé fons de l’Administració Reial i Senyorial —que comprenen documents de l’antiga administració de justícia de Sabadell i d’altres poblacions de la comarca (ss. XV-XVIII).</p>\n<p data-start=\"1252\" data-end=\"1497\" class=\"\">L’Arxiu també és receptor i fomenta la recuperació de tot tipus de fons de naturalesa privada, com poden ser fons d’empreses, d’associacions, personals i familiars. Disposa, a més, d’una col·lecció d’hemeroteca local i d’una biblioteca auxiliar.</p>\n<p data-start=\"1499\" data-end=\"1995\" class=\"\">L’AHS facilita als investigadors i al públic general la consulta i l’estudi de documents, fonamental en el dret d’accés a la informació i a la recerca històrica. A més, col·labora en activitats educatives i de divulgació patrimonial amb escoles, instituts, serveis cívics i entitats del municipi. També ofereix assessorament en matèria d’arxivística, gestió documental i història local. L’Arxiu contribueix així a preservar la memòria col·lectiva i a promoure el coneixement històric de Sabadell.</p>"
+        }
+      ]
+    },
+    {
+      "id": "1-v0240028",
+      "name": "V0240028",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "yaw": 0.03526658390921433,
+        "pitch": -0.02233389209611225,
+        "fov": 1.117963449005484
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7704955310141095,
+          "pitch": 0.18794491893927123,
+          "rotation": 0,
+          "target": "2-v0260030"
+        },
+        {
+          "yaw": -1.246960638800438,
+          "pitch": 0.1152881285471885,
+          "rotation": 0,
+          "target": "0-v0230027"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07145812812797026,
+          "pitch": -0.7427671231230306,
+          "title": "Cases i arxius, espais de memòria",
+          "text": "<h4 data-start=\"75\" data-end=\"124\" class=\"\"><br></h4>"
+        },
+        {
+          "yaw": -0.5415965008564747,
+          "pitch": -0.4802468457096527,
+          "title": "L’origen de l’Arxiu Històric de Sabadell",
+          "text": "<p data-start=\"126\" data-end=\"648\" class=\"\">L’origen de l’Arxiu Històric de Sabadell és el vell arxiu municipal, iniciat amb la formació i el desenvolupament de l’antic consell de la vila (segle XV). Els primers documents es referien als privilegis que regulaven l’organització del municipi. El 1621 el Consell General municipal adquirí la notaria feudal i l’arxiu va incorporar la part documental de la notaria: el del notari i posteriors oficials reials, assessors i altres funcionaris jurídics. Amb uns dos segles més hi havia prou per encabir-hi la documentació.</p>\n<p data-start=\"650\" data-end=\"1078\" class=\"\">El creixement de la ciutat i la complexitat administrativa del règim liberal consolidat a mitjan segle XIX van comportar un increment de la documentació local i això generà el naixement de l’hemeroteca local. Aquest creixement s’accentuà durant el segle XX, i especialment durant la dictadura franquista. A finals dels anys 1970, la documentació havia col·lapsat el vell arxiu i era dipositada en altres dependències municipals.</p>"
+        },
+        {
+          "yaw": -0.32612467721767047,
+          "pitch": -0.5607979423553751,
+          "title": "Orígens i antecedents de la Casa Ponsà",
+          "text": "<p data-start=\"1134\" data-end=\"1482\" class=\"\">Francesc de Paula Ponsà i Cantí (Sant Andreu de Palomar, 1849 - Puigcerdà, 1901) fou un industrial i comerciant que es va establir a Sabadell l’any 1871. Casat amb la barcelonina Emília Pons Armengol, sabem que feu fortuna com a soci de diverses societats, com Ponsà i Susa, Salvà i Ponsà, de Terrassa, o fons i Ponsà, amb seu a Coimbra (Portugal).</p>\n<p data-start=\"1484\" data-end=\"1745\" class=\"\">L’any 1891 va encarregar a l’arquitecte sabadellenc Gabriel Borrell Cardona la construcció d’aquest habitatge familiar, sobre una antiga vinya coneguda com Can Casanovas i Amat. La construcció es va completar l’any 1892 i la família es traslladà al nou edifici.</p>\n<p data-start=\"1747\" data-end=\"2044\" class=\"\">Fins a la fi de la Guerra Civil, a finals dels anys 1930, els hereus de Ponsà van habitar l’edifici, però molts dels seus béns van ser cedits o venuts. A la dècada de 1950, l’edifici acollí l’Estació d’Estiu de la Concepció. De nou, a finals de 1970 que va servir de casa de colònies de Sant Joan.</p>"
+        },
+        {
+          "yaw": 0.041881431079080045,
+          "pitch": -0.5831265409043667,
+          "title": "Dues històries que s’ajunten",
+          "text": "<p data-start=\"2090\" data-end=\"2509\" class=\"\">L’any 1978, en plena transició democràtica, un informe del Col·legi de Doctors i Llicenciats de Sabadell aconsellava la necessitat de dotar de noves instal·lacions i recursos humans l’arxiu històric i administratiu. Fou així que el 1980 l’Ajuntament va comprar la Casa Ponsà als descendents de la família per un total de 19.000.000 de pessetes, amb la voluntat explícita de rehabilitar l’espai per a funcions culturals.</p>\n<p data-start=\"2511\" data-end=\"2811\" class=\"\">L’AHS es va inaugurar l’abril de 1983 i va aplegar tota la documentació que es conservava a les golfes de l’Ajuntament i altres dependències municipals, i d’altra banda va iniciar una tasca proactiva en la captació de fons, especialment d’empreses tèxtils que havien fet fallida durant els anys 1970.</p>"
+        },
+        {
+          "yaw": -0.5512740545976857,
+          "pitch": -0.1887884044595296,
+          "title": "Title",
+          "text": "https://images.app.goo.gl/NAd1RWNwmUjqFvM19"
+        }
+      ]
+    },
+    {
+      "id": "2-v0260030",
+      "name": "V0260030",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.529863555731678,
+          "pitch": 0.08244777534267911,
+          "rotation": 0,
+          "target": "3-v0280034"
+        },
+        {
+          "yaw": -2.9989301332169873,
+          "pitch": 0.27815981288876834,
+          "rotation": 1.5707963267948966,
+          "target": "1-v0240028"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-v0280034",
+      "name": "V0280034",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2928373650879976,
+          "pitch": -0.47630268640984674,
+          "rotation": 0,
+          "target": "4-v0290036"
+        },
+        {
+          "yaw": 0.16998161061389716,
+          "pitch": 0.522323392501411,
+          "rotation": 0,
+          "target": "2-v0260030"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-v0290036",
+      "name": "V0290036",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7232572345229702,
+          "pitch": -0.08580299556806281,
+          "rotation": 0,
+          "target": "5-v0300040"
+        },
+        {
+          "yaw": -0.7195701769505192,
+          "pitch": 0.6095525178421077,
+          "rotation": 5.497787143782138,
+          "target": "3-v0280034"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-v0300040",
+      "name": "V0300040",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05127523348859775,
+          "pitch": 0.35858175610758103,
+          "rotation": 0,
+          "target": "6-v0310041"
+        },
+        {
+          "yaw": 1.6694741379456701,
+          "pitch": 0.5543172278002935,
+          "rotation": 0,
+          "target": "4-v0290036"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-v0310041",
+      "name": "V0310041",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0336283092425322,
+          "pitch": 0.26988045612486466,
+          "rotation": 0,
+          "target": "7-v0320044"
+        },
+        {
+          "yaw": -1.7638781059931752,
+          "pitch": 0.3294092761927381,
+          "rotation": 0,
+          "target": "9-v0350053"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-v0320044",
+      "name": "V0320044",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1067905133364544,
+          "pitch": 0.0711343507739528,
+          "rotation": 0,
+          "target": "8-v0340051"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-v0340051",
+      "name": "V0340051",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4923526309475132,
+          "pitch": 0.3402731669790189,
+          "rotation": 0,
+          "target": "6-v0310041"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-v0350053",
+      "name": "V0350053",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11777459000562729,
+          "pitch": -0.1767537078036714,
+          "rotation": 0,
+          "target": "10-v0360056"
+        },
+        {
+          "yaw": 1.7242664333984896,
+          "pitch": 0.18049390648917196,
+          "rotation": 5.497787143782138,
+          "target": "5-v0300040"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-v0360056",
+      "name": "V0360056",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06725604505118632,
+          "pitch": -0.4238809020623009,
+          "rotation": 0,
+          "target": "11-v0370059"
+        },
+        {
+          "yaw": 1.7180442969097136,
+          "pitch": 0.5851061273962124,
+          "rotation": 4.71238898038469,
+          "target": "9-v0350053"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-v0370059",
+      "name": "V0370059",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.019242597230704206,
+          "pitch": -0.23906432096524455,
+          "rotation": 1.5707963267948966,
+          "target": "12-v0380062"
+        },
+        {
+          "yaw": 1.8227402053671096,
+          "pitch": 0.640026974126922,
+          "rotation": 5.497787143782138,
+          "target": "10-v0360056"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-v0380062",
+      "name": "V0380062",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.009585432492588808,
+          "pitch": 0.1962951411364635,
+          "rotation": 0,
+          "target": "13-v0390067"
+        },
+        {
+          "yaw": 2.013668665219843,
+          "pitch": 0.6340429132133156,
+          "rotation": 4.71238898038469,
+          "target": "11-v0370059"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-v0390067",
+      "name": "V0390067",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1410,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.132601661203542,
+          "pitch": 0.12706029990162016,
+          "rotation": 4.71238898038469,
+          "target": "12-v0380062"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "AHS",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
